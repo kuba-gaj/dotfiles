afa1a5ff6ee1d29f388ab9fd65c4dc0efbf60fe3
Kuba Gaj <kuba@qubutech.com>
2019-06-16 10:00:35 +0100

    use fd instead of ag

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,5 +1,7 @@
 # FZF respect version control ignore files excluding hidden files
-export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+export FZF_DEFAULT_COMMAND='fd --type file --hidden --exclude .git --color always'
+export FZF_CTRL_T COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_DEFAULT_OPTS='--ansi'
 
 source /usr/share/fzf/key-bindings.zsh
 # source /usr/share/fzf/completion.zsh
