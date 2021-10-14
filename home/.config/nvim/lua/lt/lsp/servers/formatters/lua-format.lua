6832e1d5c9cea29fddc06eada25bb84407df704a
Kuba G <kuba@trint.com>
2021-10-14 15:32:40 +0100

    shallow symlinking

--- home/.config/nvim/lua/lt/lsp/servers/formatters/lua-format.lua
+++
@@ -1,6 +0,0 @@
-local luaformat  = {
-  formatCommand = 'lua-format -i --double-quote-to-single-quote',
-  formatStdin = true
-}
-
-return luaformat
--- home/.config/nvim/lua/lt/window/remaps.lua
+++
@@ -1,7 +0,0 @@
--- Use s instead of <C-w> to handle windows
-nnoremap('s', '<C-w>', 'remap', 'remap_s', 'Remaps s to handle windows')
-
-nnoremap('<leader>w+', '<cmd>vertical resize +5<CR>', 'window',
-         'window_increase_size', 'Increase window size')
-nnoremap('<leader>w-', '<cmd>vertical resize -5<CR>', 'window',
-         'window_decrease_size', 'Decrease window size')
