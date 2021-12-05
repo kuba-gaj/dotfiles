fdbe63e2f22c3054fd9d62dc9e6e6dc93ac1b40a
Kuba G <kuba@trint.com>
2021-12-05 21:11:27 +0000

    use ctrl for window nav

--- nvim/lua/lt/window/remaps.lua
+++ nvim/lua/lt/window/remaps.lua
@@ -1,5 +1,8 @@
--- Use s instead of <C-w> to handle windows
-nnoremap('s', '<C-w>', 'remap', 'remap_s', 'Remaps s to handle windows')
+-- Split navigation
+nnoremap('<C-h>', '<C-w>h', 'remap', 'remap_<C-h>', 'Remaps <C-h> to handle windows')
+nnoremap('<C-j>', '<C-w>j', 'remap', 'remap_<C-j>', 'Remaps <C-j> to handle windows')
+nnoremap('<C-k>', '<C-w>k', 'remap', 'remap_<C-k>', 'Remaps <C-k> to handle windows')
+nnoremap('<C-l>', '<C-w>l', 'remap', 'remap_<C-l>', 'Remaps <C-l> to handle windows')
 
 nnoremap('<leader>w+', '<cmd>vertical resize +5<CR>', 'window',
          'window_increase_size', 'Increase window size')
