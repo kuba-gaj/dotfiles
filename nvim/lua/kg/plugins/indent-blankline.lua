130865dcad6db364d8a53d9fdd189c2fb619b9a9
Kuba G <kuba@trint.com>
2022-03-20 14:39:27 +0000

    lazy loading indent-blankline

--- nvim/lua/kg/plugins/indent-blankline.lua
+++ nvim/lua/kg/plugins/indent-blankline.lua
@@ -1,4 +1,10 @@
-require("indent_blankline").setup({
+local present, indent_blankline = pcall(require, "indent-blankline")
+
+if not present then
+	return
+end
+
+indent_blankline.setup({
 	filetype_exclude = { "alpha", "lsp-installer", "packer" },
 	show_current_context = true,
 	show_current_context_start = true, -- underline first line
