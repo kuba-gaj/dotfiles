248704f2c9d4afb1eca503368d445eeb786d0b39
Kuba G <kuba@trint.com>
2024-06-14 20:47:05 +0100

    Remove lazyvim lock and some cleanup

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -6,72 +6,23 @@ return {
     servers = {
       basedpyright = {
         disableOrganizeImports = true, -- using Ruff
-        -- capabilities = (function()
-        --   require("lazyvim.util").lsp.on_attach(function(client)
-        --     if client.name == "pyright" or client.name == "basedpyright" then
-        --       client.server_capabilities.publishDiagnostics.tagSupport.valueSet = { 2 }
-        --     end
-        --   end)
-        --
-        --   -- local capabilities = vim.lsp.protocol.make_client_capabilities()
-        --   -- capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
-        --   -- return capabilities
-        -- end)(),
       },
       pyright = {
         enabled = false,
         disableOrganizeImports = true, -- using Ruff
-        -- capabilities = (function()
-        --   require("lazyvim.util").lsp.on_attach(function(client)
-        --     if client.name == "pyright" or client.name == "basedpyright" then
-        --       client.server_capabilities.publishDiagnostics.tagSupport.valueSet = { 2 }
-        --     end
-        --   end)
-        --
-        --   -- local capabilities = vim.lsp.protocol.make_client_capabilities()
-        --   -- capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
-        --   -- return capabilities
-        -- end)(),
       },
-      -- python = {
-      -- analysis = {
-      -- Ignore all files for analysis to exclusively use Ruff for linting
-      -- ignore = { "*" },
-      -- typeCheckingMode = "off", -- using mypy
-      -- },
-      -- },
-    },
-    inlay_hints = { enabled = true },
-    maxTsServerMemory = 16300,
-    tsserver = {
-      maxTsServerMemory = 16300,
-      init_options = {
-        maxTsServerMemory = 16300,
-      },
-      settings = {
-        maxTsServerMemory = 16300,
-        typescript = {
-          inlayHints = {
-            includeInlayParameterNameHints = "all",
-            includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-            includeInlayFunctionParameterTypeHints = true,
-            includeInlayVariableTypeHints = true,
-            includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-            includeInlayPropertyDeclarationTypeHints = true,
-            includeInlayFunctionLikeReturnTypeHints = true,
-            includeInlayEnumMemberValueHints = true,
+      vtsls = {
+        settings = {
+          maxTsServerMemory = 16300,
+          vtsls = {
+            maxTsServerMemory = 16300,
           },
-        },
-        javascript = {
-          inlayHints = {
-            includeInlayParameterNameHints = "all",
-            includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-            includeInlayFunctionParameterTypeHints = true,
-            includeInlayVariableTypeHints = true,
-            includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-            includeInlayPropertyDeclarationTypeHints = true,
-            includeInlayFunctionLikeReturnTypeHints = true,
-            includeInlayEnumMemberValueHints = true,
+          typescript = {
+            maxTsServerMemory = 16300,
+            tsserver = {
+              useSeparateSyntaxServer = false,
+              useSyntaxServer = "never",
+            },
           },
         },
       },
