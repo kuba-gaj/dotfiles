1422e68b517e3ad999b94321a15b0d93778132ed
Kuba G <kuba@trint.com>
2024-06-15 11:26:08 +0100

    optimize typescript lsp and some cleanup

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,8 +1,7 @@
 return {
   "neovim/nvim-lspconfig",
-  -- enabled = true,
   opts = {
-    -- python tweaks
+    inlay_hints = { enabled = false },
     servers = {
       basedpyright = {
         disableOrganizeImports = true, -- using Ruff
@@ -13,19 +12,49 @@ return {
       },
       vtsls = {
         settings = {
-          maxTsServerMemory = 16300,
           vtsls = {
-            maxTsServerMemory = 16300,
+            autoUseWorkspaceTsdk = true,
+            experimental = {
+              completion = {
+                enableServerSideFuzzyMatch = true,
+                -- entriesLimit = 100,
+              },
+            },
           },
           typescript = {
-            maxTsServerMemory = 16300,
+            preferences = {
+              -- "Enable/disable searching `package.json` dependencies for available auto imports."
+              -- "Search dependencies based on estimated performance impact.",
+              -- "Never search dependencies."
+              -- "Always search dependencies.",
+              includePackageJsonAutoImports = "auto", -- auto, on, off
+              -- Specify glob patterns of files to exclude from auto imports. Relative paths are resolved relative to the workspace root. Patterns are evaluated using tsconfig.json [`exclude`](https://www.typescriptlang.org/tsconfig#exclude) semantics.
+              -- autoImportFileExcludePatterns = [],
+            },
             tsserver = {
-              useSeparateSyntaxServer = false,
-              useSyntaxServer = "never",
+              maxTsServerMemory = 16000,
+              useSyntaxServer = "auto", --auto,always,never
+              useSeparateSyntaxServer = true,
+              -- log = "/home/kuba/dev/tsserver.log",
+              -- using unofficial build with pointer compression enabled
+              -- https://github.com/typescript-language-server/typescript-language-server/issues/472#issuecomment-1195166893
+              -- https://github.com/yioneko/vtsls/issues/136#issuecomment-1919246219
+              nodePath = "/home/kuba/dev/node-v22.1.0-linux-x64-pointer-compression/bin/node",
             },
           },
         },
       },
+    },
+    setup = {
+      eslint = function()
+        require("lazyvim.util").lsp.on_attach(function(client)
+          if client.name == "eslint" then
+            client.server_capabilities.documentFormattingProvider = true
+          elseif client.name == "tsserver" or client.name == "vtsls" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end)
+      end,
     },
   },
 }
--- nvim/lua/plugins/typescript-tools.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "pmizio/typescript-tools.nvim",
-  enabled = false,
-  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {
-    tsserver_max_memory = 8096,
-    -- tsserver_max_memory = "auto",
-  },
-}
