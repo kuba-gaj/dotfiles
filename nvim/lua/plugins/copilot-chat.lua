209d95cba7f70f68471b5ad0bc37eedcde2ebd7e
Kuba G <kuba@trint.com>
2024-06-24 18:27:12 +0100

    copilot-chat tweaks

---
+++ nvim/lua/plugins/copilot-chat.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "folke/edgy.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.right = opts.right or {}
+      for i, v in ipairs(opts.right) do
+        if v.ft == "copilot-chat" then
+          opts.right[i] = {
+            ft = "copilot-chat",
+            title = "Copilot Chat",
+            size = { width = 70 },
+          }
+          break
+        end
+      end
+    end,
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      show_help = false,
+      auto_follow_cursor = false,
+      show_folds = true,
+      auto_insert_mode = false,
+      clear_chat_on_new_prompt = true,
+      highlight_selection = true,
+      context = nil,
+      mappings = {
+        reset = {
+          normal = "R",
+          insert = false,
+        },
+      },
+    },
+  },
+}
