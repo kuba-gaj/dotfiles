bca04e19d3c6b6171365198283732885c254f08a
Kuba G <kuba@trint.com>
2024-07-19 09:51:40 +0100

    nvim add inlay hints and multicursor

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,100 +1,107 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    inlay_hints = { enabled = false },
-    servers = {
-      jsonls = {
-        settings = {
-          json = {
-            format = {
-              enable = false,
-            },
-          },
-        },
-      },
-      basedpyright = {
-        disableOrganizeImports = true, -- using Ruff
-      },
-      pyright = {
-        enabled = false,
-        disableOrganizeImports = true, -- using Ruff
-      },
-      vtsls = {
-        -- monorepo fix, also looks like it fixes memory issues
-        -- i override vim.g.root_spec to have cwd/root pickers working correctly
-        root_dir = function()
-          local lazyvimRoot = require("lazyvim.util.root")
-          -- use git for lsp root or fallback to default option vim.g.root_spec
-          return lazyvimRoot.git() or lazyvimRoot.detect()
-        end,
-        settings = {
-          vtsls = {
-            autoUseWorkspaceTsdk = true,
-            experimental = {
-              completion = {
-                enableServerSideFuzzyMatch = true,
-                -- entriesLimit = 25,
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = true },
+      servers = {
+        jsonls = {
+          settings = {
+            json = {
+              format = {
+                enable = false,
               },
             },
           },
-          typescript = {
-            preferences = {
-              -- "Enable/disable searching `package.json` dependencies for available auto imports."
-              -- "Search dependencies based on estimated performance impact.",
-              -- "Never search dependencies."
-              -- "Always search dependencies.",
-              includePackageJsonAutoImports = "auto", -- auto, on, off
-              -- Specify glob patterns of files to exclude from auto imports. Relative paths are resolved relative to the workspace root. Patterns are evaluated using tsconfig.json [`exclude`](https://www.typescriptlang.org/tsconfig#exclude) semantics.
-              -- autoImportFileExcludePatterns = { "*trint-operation*" },
+        },
+        basedpyright = {
+          disableOrganizeImports = true, -- using Ruff
+        },
+        pyright = {
+          enabled = false,
+          disableOrganizeImports = true, -- using Ruff
+        },
+        vtsls = {
+          -- monorepo fix, also looks like it fixes memory issues
+          -- i override vim.g.root_spec to have cwd/root pickers working correctly
+          root_dir = function()
+            local lazyvimRoot = require("lazyvim.util.root")
+            -- use git for lsp root or fallback to default option vim.g.root_spec
+            return lazyvimRoot.git() or lazyvimRoot.detect()
+          end,
+          settings = {
+            vtsls = {
+              autoUseWorkspaceTsdk = true,
+              experimental = {
+                completion = {
+                  enableServerSideFuzzyMatch = true,
+                  -- entriesLimit = 25,
+                },
+              },
             },
-            tsserver = {
-              maxTsServerMemory = 8000,
-              -- deprecated in favor of useSyntaxServer
-              useSeparateSyntaxServer = false,
-              -- Controls if TypeScript launches a dedicated server to more quickly handle syntax related operations, such as computing code folding.
-              useSyntaxServer = "auto", --auto,always,never
-              -- in /tmp/tsserver-log-*/tsserver.log
-              log = "off", -- off, terse, normal, verbose
-              -- use cmd in setup instead to get rid of the tsserver popup
-              -- nodePath = "/home/kuba/dev/node-v22.1.0-linux-x64-pointer-compression/bin/node",
+            typescript = {
+              preferences = {
+                -- "Enable/disable searching `package.json` dependencies for available auto imports."
+                -- "Search dependencies based on estimated performance impact.",
+                -- "Never search dependencies."
+                -- "Always search dependencies.",
+                includePackageJsonAutoImports = "auto", -- auto, on, off
+                -- Specify glob patterns of files to exclude from auto imports. Relative paths are resolved relative to the workspace root. Patterns are evaluated using tsconfig.json [`exclude`](https://www.typescriptlang.org/tsconfig#exclude) semantics.
+                -- autoImportFileExcludePatterns = { "*trint-operation*" },
+              },
+              tsserver = {
+                maxTsServerMemory = 8000,
+                -- deprecated in favor of useSyntaxServer
+                useSeparateSyntaxServer = false,
+                -- Controls if TypeScript launches a dedicated server to more quickly handle syntax related operations, such as computing code folding.
+                useSyntaxServer = "auto", --auto,always,never
+                -- in /tmp/tsserver-log-*/tsserver.log
+                log = "off", -- off, terse, normal, verbose
+                -- use cmd in setup instead to get rid of the tsserver popup
+                -- nodePath = "/home/kuba/dev/node-v22.1.0-linux-x64-pointer-compression/bin/node",
+              },
             },
           },
         },
       },
-    },
-    setup = {
-      jsonls = function()
-        LazyVim.lsp.on_attach(function(client, _)
-          if client.name == "jsonls" then
-            client.server_capabilities.documentFormattingProvider = false
-          end
-        end)
-      end,
-      eslint = function()
-        LazyVim.lsp.on_attach(function(client, _)
-          if client.name == "eslint" then
-            client.server_capabilities.documentFormattingProvider = true
-          elseif client.name == "tsserver" or client.name == "vtsls" then
-            client.server_capabilities.documentFormattingProvider = false
-          end
-        end)
-      end,
-      vtsls = function(_, opts)
-        -- using unofficial build with pointer compression enabled
-        -- https://github.com/typescript-language-server/typescript-language-server/issues/472#issuecomment-1195166893
-        -- https://github.com/yioneko/vtsls/issues/136#issuecomment-1919246219
-        -- https://v8.dev/blog/pointer-compression
-        local nodePath = "/home/kuba/dev/node-v22.1.0-linux-x64-pointer-compression/bin/node"
-        local masonRoot = vim.env.MASON or (vim.fn.stdpath("data") .. "/mason")
-        local vtslsPath = masonRoot .. "/bin/vtsls"
+      setup = {
+        jsonls = function()
+          LazyVim.lsp.on_attach(function(client, _)
+            if client.name == "jsonls" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+        eslint = function()
+          LazyVim.lsp.on_attach(function(client, _)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" or client.name == "vtsls" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+        vtsls = function(_, opts)
+          -- using unofficial build with pointer compression enabled
+          -- https://github.com/typescript-language-server/typescript-language-server/issues/472#issuecomment-1195166893
+          -- https://github.com/yioneko/vtsls/issues/136#issuecomment-1919246219
+          -- https://v8.dev/blog/pointer-compression
+          local nodePath = "/home/kuba/dev/node-v22.1.0-linux-x64-pointer-compression/bin/node"
+          local masonRoot = vim.env.MASON or (vim.fn.stdpath("data") .. "/mason")
+          local vtslsPath = masonRoot .. "/bin/vtsls"
 
-        opts.cmd = {
-          nodePath,
-          vtslsPath,
-          "--stdio",
-        }
-        -- return opts
-      end,
+          opts.cmd = {
+            nodePath,
+            vtslsPath,
+            "--stdio",
+          }
+          -- return opts
+        end,
+      },
     },
+  },
+  {
+    "chrisgrieser/nvim-lsp-endhints",
+    event = "LspAttach",
+    opts = {}, -- required, even if empty
   },
 }
