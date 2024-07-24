d9ea2f5f4008cb75478fea41220632995eb47b72
Kuba G <kuba@trint.com>
2024-07-24 15:31:13 +0100

    python is back

--- zsh/rc.d/062-dev.zsh
+++ zsh/rc.d/062-dev.zsh
@@ -18,9 +18,14 @@ export GOBIN=$GOPATH/bin
 path+=("$GOBIN")
 
 # python
-# export PYENV_ROOT="$HOME/.pyenv"
-# [[ -d $PYENV_ROOT/bin ]] && path+="$PYENV_ROOT/bin:$PATH"
-# znap eval pyenv 'pyenv init -'
+# vitualenvwrapper
+export WORKON_HOME=$HOME/.virtualenvs
+source '/usr/bin/virtualenvwrapper.sh'
+export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
+
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && path=("$PYENV_ROOT/bin" $path)
+znap eval pyenv 'pyenv init -'
 znap eval pipx-argcomplete 'register-python-argcomplete pipx'
 
 # lua5.1 is in /usr/local/bin
